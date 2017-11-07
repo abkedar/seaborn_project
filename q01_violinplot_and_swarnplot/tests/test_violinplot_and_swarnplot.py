1afb9f9d9c89840a06f0833d130c0181efcd5ba9
Alex Anthony <alex19427@gmail.com>
2017-11-07 18:58:33 +0530

    Update test_violinplot_and_swarnplot.py

--- q01_violinplot_and_swarnplot/tests/test_violinplot_and_swarnplot.py
+++ q01_violinplot_and_swarnplot/tests/test_violinplot_and_swarnplot.py
@@ -1,6 +1,11 @@
 from unittest import TestCase
-
+from ..build import plot
+from inspect import getargspec
 
 class TestPlot_violinplot_and_swarnplot(TestCase):
     def test_violinplot_and_swarnplot(self):
-        self.assertTrue(True)
+                # Input parameters tests
+        args = getargspec(plot)
+        self.assertEqual(len(args[0]), 4, "Expected argument(s) %d, Given %d" % (4, len(args[0])))
+        self.assertEqual(args[3], None, "Expected default values do not match given default values")
+		
